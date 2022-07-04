2633742291657fd988c13877d64bbe4020cab897
leoarthurwall <leowall93@gmail.com>
2022-07-04 17:23:32 +0100

    automatic check with netlify

--- deploy/src/App.js
+++ deploy/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Hello there!</h1>
       <button>click me</button>
       <h2>Hello again</h2>
+      <p>this is to check that netlify updates automtically</p>
     </div>
   );
 }
